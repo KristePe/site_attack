1e96e5495267b7e42ba622b68ab92c2c30e3c856
KristePe <biskyte@hotmail.com>
2022-03-01 13:11:36 +0200

    renew

---
+++ src/components/toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Toolbar = () => {
+    return (
+        <div className="toolbar">
+            <div>Site Name </div>
+            <div>Status</div>
+            <div>Requests</div>
+        </div>
+    );
+};
+
+export default Toolbar;
